a9e60c81358d80e9e69d8b0ebad2f28371912341
maomaomao <534863795@qq.com>
2025-01-10 17:01:54 +0800

    第三次提交

---
+++ git-test-csharp.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace git_test_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//用于测试git的使用
