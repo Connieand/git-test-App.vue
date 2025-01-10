5f2719546755a988c291c0e513814b957884f6d7
maomaomao <534863795@qq.com>
2025-01-10 16:54:58 +0800

    这次提交仅用作测试

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- 路由占位符 -->
+    <!-- <keep-alive :include="['PlanProgramGenerate']"> -->
+    <keep-alive :include="['PlanSchemeVerification']">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+.redWarning p {
+  color: red;
+  padding: 0;
+  margin: 0;
+}
+</style>
+//Git-TEST仅用于测试
